7fd2332e1da0fff413d1ef98fb088c0fc21c3b8f
pres <mateospres@gmail.com>
2024-10-25 16:12:10 -0400

    art
    Tileset update

---
+++ assets/pres/littlemage.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var littlemage: AnimatedSprite2D = $Littlemage
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	littlemage.play("idle_right")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
