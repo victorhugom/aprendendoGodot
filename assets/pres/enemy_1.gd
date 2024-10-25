4fff59e04473ce9c87f53290bc280185b9a1e025
pres <mateospres@gmail.com>
2024-10-25 16:53:02 -0400

    art
    added enemy sprite
    added littlemage base sprite
    props update

--- assets/pres/enemy_1.gd
+++ assets/pres/enemy_1.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-@onready var torch: AnimatedSprite2D = $"Torch"
+@onready var enemy1: AnimatedSprite2D = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	torch.play("flame")
+	enemy1.play("idle_left")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
