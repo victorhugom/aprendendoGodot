fd9fc6fe3c921ab1e2e001566ef1a5dcedcd3987
pres <mateospres@gmail.com>
2024-10-25 16:24:23 -0400

    art
    assets update

--- assets/pres/littlemage.gd
+++ assets/pres/littlemage.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var littlemage: AnimatedSprite2D = $Littlemage
+@onready var littlemage: AnimatedSprite2D = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
