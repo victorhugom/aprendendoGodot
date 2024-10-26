7104fda00cc4e482102bccca0c17293da7fd39a8
Victor Hugo  Marques <victorhm@outlook.com>
2024-10-26 09:33:57 -0300

    Cleanup assets
    Cleanup asset folder

--- npc/enemy.gd
+++ npc/enemy.gd
@@ -1,7 +1,9 @@
 extends "res://npc/npc.gd"
 
 class_name Enemy
+@onready var animation_sprite_2d: AnimatedSprite2D = $AnimationSprite2D
 
 func _ready() -> void:
 	super._ready()
 	track_node(%Player)
+	animation_sprite_2d.play("idle_down")
