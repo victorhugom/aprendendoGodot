85d47e1587b5c2d4738d05f57f3baba1478ce8af
Victor Hugo  Marques <victorhm@outlook.com>
2024-10-27 23:02:11 -0300

    New nav
    Nav using navigation agent

--- npc/enemy.gd
+++ npc/enemy.gd
@@ -1,4 +1,4 @@
-class_name Enemy extends "res://npc/npc.gd"
+class_name Enemy extends CharacterBody2D
 
 const PROJECTILE = preload("res://player/projectile.tscn")
 const PROJECTILE_CONFIG = preload("res://player/projectile/enemy_eye_projectile.tres")
@@ -9,28 +9,37 @@ const PROJECTILE_CONFIG = preload("res://player/projectile/enemy_eye_projectile.
 @onready var attack_timer: Timer = $AttackTimer
 @onready var projectile_position: Node2D = $ProjectilePosition
 
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var player: Player = %Player
+
+@export var speed = 16*3
+
 var life = 3
 var is_being_hit = false
 var is_dying = false
 var is_attacking = false
 var direction = "left"
+var is_seeing_player = false
 
 var last_hit_knock_back_direction: Vector2
 var last_hit_knock_back_power: int
 
 func _ready() -> void:
-	super._ready()
-	track_node(%Player)
+	pass
 	
 func _physics_process(delta: float) -> void:
-	
-	stop = player_trace.is_colliding()
 	
 	if is_dying || is_being_hit || is_attacking: 
 		return
 	
-	super._physics_process(delta)
+	is_seeing_player = player_trace.is_colliding()
+	if is_seeing_player == false: #only moves if not seeing player
+		var next_path_position = navigation_agent_2d.get_next_path_position()
+		var current_position = global_position
+		velocity = current_position.direction_to(next_path_position) * speed
+		move_and_slide()
 	
+
 	if velocity.x < 0:
 		enemy.flip_h = false
 		direction = "left"
@@ -44,6 +53,9 @@ func _physics_process(delta: float) -> void:
 		animation_player.play("walk_left")
 	else:
 		animation_player.play("idle_left")
+		
+func follow_player() -> void:
+	navigation_agent_2d.target_position = player.global_position
 	
 func damage(hurt_points: int, damage_type: Enums.ELEMENTS, knock_back_direction = Vector2(0,0), knock_back_power = 1500) -> void:
 	
@@ -62,6 +74,15 @@ func damage(hurt_points: int, damage_type: Enums.ELEMENTS, knock_back_direction 
 	else:
 		is_being_hit = true
 		animation_player.play("hit")
+
+func knockBack(knock_back_direction: Vector2, knock_back_power: int):
+	var knockBackDirection = (knock_back_direction-velocity).normalized() * knock_back_power
+	velocity = knockBackDirection
+	move_and_slide()
+	
+func delayed_knock_back():
+	if last_hit_knock_back_direction.length() > 0:
+		knockBack(last_hit_knock_back_direction, last_hit_knock_back_power)
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "hit":
@@ -91,12 +112,10 @@ func _on_attack_timer_timeout() -> void:
 		
 		get_parent().add_sibling(projectile)
 		projectile.shoot(direction)
-		
-func knockBack(knock_back_direction: Vector2, knock_back_power: int):
-	var knockBackDirection = (knock_back_direction-velocity).normalized() * knock_back_power
-	velocity = knockBackDirection
-	move_and_slide()
-	
-func delayed_knock_back():
-	if last_hit_knock_back_direction.length() > 0:
-		knockBack(last_hit_knock_back_direction, last_hit_knock_back_power)
+
+func _on_tracking_timer_timeout() -> void:
+	follow_player()
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	#velocity = safe_velocity
+	pass
