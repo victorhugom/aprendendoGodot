5ab734b3df95b9e6ddab9cbee543d92374edbaf3
Victor Hugo  Marques <victorhm@outlook.com>
2024-11-04 10:52:49 -0300

    Landing screen
    Landing screen with start and quit options

--- gui/landing_screen.gd
+++ gui/landing_screen.gd
@@ -3,14 +3,13 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().paused = true
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _on_restart_button_pressed() -> void:
+func _on_start_button_pressed() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	get_tree().change_scene_to_file("res://main.tscn")
 
