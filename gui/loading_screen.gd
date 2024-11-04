843d921d649a516d66347456f1c5c07b355938cc
Victor Hugo  Marques <victorhm@outlook.com>
2024-11-04 14:52:10 -0300

    WIP: Loading screen
    Taking too long to load main screen

---
+++ gui/loading_screen.gd
@@ -0,0 +1,35 @@
+class_name LoadingScreen extends CanvasLayer
+
+@onready var loading_sprite: AnimatedSprite2D = $CenterContainer/LoadingSprite
+
+var loading_status : int
+var progress : Array[float]
+
+func _ready() -> void:
+	# Request to load the target scene:
+	loading_sprite.play("default")
+	
+	var parent_size = loading_sprite.get_parent().get_rect().size 
+	loading_sprite.position = parent_size / 2 - Vector2(128,128)
+	
+	ResourceLoader.load_threaded_request(Globals.next_screen)
+	
+func _process(_delta: float) -> void:
+	# Update the status:
+	loading_status = ResourceLoader.load_threaded_get_status(Globals.next_screen, progress)
+	
+	# Check the loading status:
+	match loading_status:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			print_debug("%s loading scene in bkg: %s" %[Time.get_time_string_from_system(), progress[0] * 100])
+			#progress_bar.value = progress[0] * 100 # Change the ProgressBar value
+		ResourceLoader.THREAD_LOAD_LOADED:
+			# When done loading, change to the target scene:
+			print_debug("%s: %s loaded scene in bkg" %[Time.get_time_string_from_system(), progress[0] * 100])
+			get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(Globals.next_screen))
+			print_debug("%s: changed to scene" %Time.get_time_string_from_system())
+			
+		ResourceLoader.THREAD_LOAD_FAILED:
+			# Well some error happend:
+			print_debug("Error. Could not load Resource")
+			
