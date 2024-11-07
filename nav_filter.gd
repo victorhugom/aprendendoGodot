eb53585c5b8d74fb3f443c724bff886dae9000d4
Victor Hugo  Marques <victorhm@outlook.com>
2024-11-07 11:20:11 -0300

    Nav Filter update
    Allow to set multi block nav and have multi walkable tiles

--- nav_filter.gd
+++ nav_filter.gd
@@ -1,8 +1,6 @@
-extends TileMapLayer
+class_name WalkableTileMapLayer extends TileMapLayer
 
-@onready var walls: TileMapLayer = $"../Walls"
-@onready var furniture: TileMapLayer = $"../Furniture"
-@onready var decoration: TileMapLayer = $"../Decoration"
+@export var not_walkable_tiles: Array[TileMapLayer]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,9 +12,10 @@ func _process(_delta: float) -> void:
 	
 func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
 	
-	if coords in walls.get_used_cells() || coords in furniture.get_used_cells() ||  coords in decoration.get_used_cells():
-		return true
-		
+	for tile_map in not_walkable_tiles:
+		if coords in tile_map.get_used_cells():
+			return true
+			
 	return false
 	
 func _tile_data_runtime_update(_coords: Vector2i, tile_data: TileData) -> void:
