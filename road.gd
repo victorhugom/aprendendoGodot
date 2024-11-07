5e0628cad4c9c3ecb0a4955dbc4ad55fda42a585
pres <mateospres@gmail.com>
2024-11-07 15:51:13 -0400

    art
    map update

--- road.gd
+++ road.gd
@@ -1,11 +1,11 @@
-extends Node2D
+extends TileMapLayer
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animated_sprite_2d.play("default")
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
