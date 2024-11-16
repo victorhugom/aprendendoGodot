5986c8fe5e782a59b484fb5e616733df1221e187
Victor Hugo  Marques <victorhm@outlook.com>
2024-11-16 15:27:18 -0300

    Fix: Death screen retry
    Fix retry death screen, goes to loby and don't pause, pause was breakin the laoding of new screen

--- gui/death_screen.gd
+++ gui/death_screen.gd
@@ -1,15 +1,18 @@
 extends CanvasLayer
 
+const LOADING_SCREEN = preload("res://gui/loadingScreen.tscn")
+var loading_screen: LoadingScreen
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().paused = true
+	#get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_restart_button_pressed() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	Globals.next_scence_path = "res://levels/main.tscn"
+	loading_screen = LOADING_SCREEN.instantiate()
+	Globals.next_scence_path = "res://levels/lobby.tscn"
 	get_tree().change_scene_to_packed(Globals.loading_screen)
+	queue_free()
 	
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
--- gui/landing.gd
+++ gui/landing.gd
@@ -1,13 +1,11 @@
 class_name LandingScene extends CanvasLayer
 
 const LOADING_SCREEN = preload("res://gui/loadingScreen.tscn")
-
 var loading_screen: LoadingScreen
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	loading_screen = LOADING_SCREEN.instantiate()
-	pass # Replace with function body.
 
 func _on_start_button_pressed() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
