6556116c25d09153b574bed502f7309f153abfc1
Victor Hugo  Marques <victorhm@outlook.com>
2024-11-21 16:25:14 -0300

    Small update, tweener instead of timer to shoot
    Shoots with more than 1 DPS uses tweener

--- player/character_components/shooter.gd
+++ player/character_components/shooter.gd
@@ -2,8 +2,6 @@ class_name Shooter extends Area2D
 
 const PROJECTILE = preload("res://player/projectile.tscn")
 
-@onready var timer: Timer = $Timer
-
 @export var left_marker: Marker2D
 @export var right_marker: Marker2D
 @export var up_marker: Marker2D
@@ -14,19 +12,12 @@ const PROJECTILE = preload("res://player/projectile.tscn")
 ## size of the projectile
 @export var projectile_scale:= 1
 ## delay between shoots, only works if projectile count > 1
-@export var shoot_interval: float:
-	set(value):
-		timer.wait_time = value
-	get:
-		return shoot_interval
+@export var shoot_interval:= 0.01
 
 var projectile_config: ProjectileConfig
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	timer.wait_time = minf(0.001, shoot_interval)
-
 func shoot(direction: String, projectile_count: int, target_location: Vector2 = Vector2(0, 0)):
+	
 	# Determine the position based on the direction at the time of shooting
 	var shoot_position: Vector2
 	match direction:
@@ -38,19 +29,26 @@ func shoot(direction: String, projectile_count: int, target_location: Vector2 = 
 			shoot_position = up_marker.global_position
 		"down":
 			shoot_position = down_marker.global_position
+			
+	_shoot(shoot_position, direction, target_location)
 	
-	for i in range(projectile_count):
-		var projectile = PROJECTILE.instantiate()
+	if projectile_count > 1:
+		var tween = get_tree().create_tween()
+		tween.set_loops(projectile_count - 1)
 		
-		projectile.set_config(projectile_config)
-		projectile.collision_layer = collision_layer
-		projectile.collision_mask = collision_mask
-		projectile.position = shoot_position  # Use the lock the position
-		projectile.scale = Vector2(projectile_scale, projectile_scale)
-		projectile.speed = speed
-		
-		get_tree().root.add_child(projectile)
-		projectile.shoot(direction, target_location)
-		
-		timer.start()
-		await timer.timeout
+		var shoot_callback = Callable(_shoot).bind(shoot_position, direction, target_location)
+		tween.tween_callback(shoot_callback).set_delay(shoot_interval)
+
+func _shoot(shoot_position: Vector2, direction: String, target_location:= Vector2(0, 0)):
+	
+	var projectile = PROJECTILE.instantiate()
+	
+	projectile.set_config(projectile_config)
+	projectile.collision_layer = collision_layer
+	projectile.collision_mask = collision_mask
+	projectile.position = shoot_position  # Use the lock the position
+	projectile.scale = Vector2(projectile_scale, projectile_scale)
+	projectile.speed = speed
+	
+	get_tree().root.add_child(projectile)
+	projectile.shoot(direction, target_location)
